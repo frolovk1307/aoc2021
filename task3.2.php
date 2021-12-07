576104db9590617d714dddc466fd6058154e5dc0
filin <filin@syneforge.com>
2021-12-07 19:00:56 +0200

    task 3.2

--- task3.2.php
+++ task3.2.php
@@ -2,21 +2,25 @@
 $input = file_get_contents('./input3.txt');
 $input = explode("\n", $input);
 
+$biCriteriaReducerForOxygen = fn(array $bits) => (int)(array_sum($bits) >= (count($bits) / 2));
+$biCriteriaReducerForCo2 = fn(array $bits) => (int)(array_sum($bits) < (count($bits) / 2));
+
+$reportValuesReducer = function ($values, $bitCriteriaReducer) {
+    $position = 0;
+    while (count($values) > 1) {
+        $bitCriteria = $bitCriteriaReducer(array_column($values, $position));
+        $values = array_filter($values, fn($value) => $value[$position] === $bitCriteria);
+        $position++;
+    }
+
+    return array_shift($values);
+};
+
 $matrix = array_map(fn($binaryStr) => array_map('intval', str_split($binaryStr)), $input);
 
-$oxygenRating = 0;
-$co2rating = 0;
+$oxygenRating = bindec(implode($reportValuesReducer($matrix, $biCriteriaReducerForOxygen)));
+$co2rating = bindec(implode($reportValuesReducer($matrix, $biCriteriaReducerForCo2)));
 
-$currentPosition = 0;
+$result = $oxygenRating * $co2rating;
 
-$count = array_sum(array_column($matrix, $currentPosition));
-
-$oxygenRows = [];
-$co2Rows = [];
-foreach ($matrix as $matrixRow) {
-    $bit = $matrixRow[$currentPosition];
-
-
-}
-
-//echo $result . "\n";
+echo $result . "\n";
